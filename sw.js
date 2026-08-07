f4b73714a96b1446c8ceca3be65be072693b4115
Con-bridge <viabellinicondominio@gmail.com>
2026-08-07 12:18:36 +0200

    Refactor service worker for cache and notifications

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Definiamo nuovo nome per la cache
+// Definiamo nuovo nome per la nostra cache
 const CACHE_NAME = 'condo-app-pwa-cache-v2';
 
 // Elenco dei file fondamentali da salvare per il funzionamento offline
