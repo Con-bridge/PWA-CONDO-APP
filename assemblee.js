91f1ee1d8fe2bf4dc888ac282438ea6108b0d2e3
Con-bridge <viabellinicondominio@gmail.com>
2026-08-17 19:50:21 +0200

    Modifiche del 17 luglio parte 3

--- assemblee.js
+++ assemblee.js
@@ -27,7 +27,7 @@ window.AssembleeModule = {
             </div>
         ` : '';
 
-        // DELEGHE E TESSERA QR VISIBILI SOLO SE NON SI È AMMINISTRATORI
+        // DELEGHE + TESSERA QR VISIBILI SOLO SE NON SI È AMMINISTRATORI
         const delegheButton = isCondomino ? `
             <div onclick="navigateTo('assemblea_deleghe')" class="dashboard-item">
                 <div class="dashboard-card" style="position: relative;">
