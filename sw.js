432a8b400ee7170a14cde2b65c4e1f5af1df85ca
Con-bridge <viabellinicondominio@gmail.com>
2026-08-27 00:47:07 +0200

    Modifiche del 27 agosto sperimentali x velocità

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Definiamo nuovo nome per la nostra cache (BUMP v12)
+// Definiamo nuovo nome per la cache (BUMP v12)
 const CACHE_NAME = 'condo-app-pwa-cache-v12';
 
 // Elenco dei file fondamentali da salvare per il funzionamento offline
