cfbb411fd3e53dc23e3f1d897ae29c02769c490d
Con-bridge <viabellinicondominio@gmail.com>
2026-09-01 08:18:09 +0200

    Modifiche del 1 settembre 2026

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Definiamo nuovo nome per la cache (BUMP v31)
-const CACHE_NAME = 'condo-app-pwa-cache-v31';
+// Definiamo nuovo nome per la cache (BUMP v44)
+const CACHE_NAME = 'condo-app-pwa-cache-v44';
 
 // Elenco dei file fondamentali da salvare per il funzionamento offline
 const URLS_TO_CACHE = [
