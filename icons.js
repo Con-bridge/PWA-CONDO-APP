afc719aaf22053d1f28cf072a12305e3659bdce6
Con-bridge <viabellinicondominio@gmail.com>
2026-09-09 15:52:51 +0200

    Add upload function to icons.js

--- icons.js
+++ icons.js
@@ -38,6 +38,9 @@
         download: (size = 16, stroke = 2, extraClass = '', extraStyle = '') =>
             createSvg('<path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line>', size, stroke, extraClass, extraStyle),
 
+        upload: (size = 16, stroke = 2, extraClass = '', extraStyle = '') =>
+            createSvg('<path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="17 8 12 3 7 8"></polyline><line x1="12" y1="3" x2="12" y2="15"></line>', size, stroke, extraClass, extraStyle),
+
         fileText: (size = 16, stroke = 2, extraClass = '', extraStyle = '') =>
             createSvg('<path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline>', size, stroke, extraClass, extraStyle),
 
