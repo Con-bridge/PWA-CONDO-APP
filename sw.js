475a844bf134df79a49c92799ee04dc7c871d45b
Con-bridge <viabellinicondominio@gmail.com>
2026-09-11 15:13:09 +0200

    Refactor service worker for cache and notifications

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Definiamo nuovo nome per la nostra cache (BUMP v68 - modale fullscreen uniforma)
+// Definiamo nuovo nome per la cache (BUMP v68 - modale fullscreen uniforma)
 const CACHE_NAME = 'condo-app-pwa-cache-v68';
 
 // Elenco dei file fondamentali da salvare per il funzionamento offline
